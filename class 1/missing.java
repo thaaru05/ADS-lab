313c50b73c16761cdcf0b29f422af70e3c45427f
Thaarugeshwari <121503925+thaaru05@users.noreply.github.com>
2024-02-15 14:13:44 +0530

    Update missing.java

--- class 1/missing.java
+++ class 1/missing.java
@@ -1,4 +1,12 @@
-
+/*
+Given an array arr[] of size N-1 with integers in the range of [1, N], the task is to find the missing
+number from the first N integers.
+Note: There are no duplicates in the list.
+Examples:
+Input: arr[] = {1, 2, 4, 6, 3, 7, 8}
+Output: 5
+Explanation: Here the size of the array is 7, so the range will be [1, 8]. The missing number
+between 1 to 8 is 5*/
 public class missing{
     public static void main(String[] args) {
         int[] arr = {1, 2, 4, 6, 3, 7, 8}; // Example array, you can replace it with any input array
