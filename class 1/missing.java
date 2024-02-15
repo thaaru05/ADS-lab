caaa0f9c54308e58e831eab535a82175fbf4140e
Thaarugeshwari <121503925+thaaru05@users.noreply.github.com>
2024-02-15 14:22:59 +0530

    Update missing.java
    done

--- class 1/missing.java
+++ class 1/missing.java
@@ -8,19 +8,16 @@ Output: 5
 Explanation: Here the size of the array is 7, so the range will be [1, 8]. The missing number
 between 1 to 8 is 5*/
 public class missing{
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 4, 6, 3, 7, 8}; // Example array, you can replace it with any input array
-
-        int n = arr.length + 1; // Size of original array
-
-        int expectedSum = n * (n + 1) / 2; // Sum of first N natural numbers
-        int actualSum = 0; // Sum of elements in the array
-
-        for (int num : arr) {
-            actualSum += num;
+    public static void main(String args[])
+    {
+        int arr[]={1,2,4,6,3,7,8};
+        int n=arr.length+1;
+        int sum=n*(n+1)/2;
+        int actualsum=0;
+        for(int num:arr)
+        {
+            actualsum+=num;
         }
-
-        int missingNumber = expectedSum - actualSum;
-        System.out.println("The missing number is: " + missingNumber);
+        System.out.println("The missing number is:"+(sum-actualsum));
     }
 }
